2b8d8e6a38186d4e043372d236974f459f255984
rsametk <rsmtkcky@gmail.com>
2023-06-30 21:37:29 -0400

    whole solution of project

---
+++ solution/var.tf
@@ -0,0 +1,12 @@
+eariable "git-name" {
+  default = "repo name"
+}
+variable "git-token" {
+  default = "---"  #token
+}
+variable "key-name" {
+  default = "my_keypair"
+}
+variable "hosted-zone" {
+  default = "domain name"
+}
